de19726906ec01aa1ba2252a70ed8861275c7f89
ccredrock <ccredrock@163.com>
2017-03-07 10:54:46 +0800

    shell sort

--- code/sum_sort_algorithm.c
+++ code/sum_sort_algorithm.c
@@ -4,8 +4,7 @@
 #include <string.h>
 #include <sys/time.h>
 
-#define SIZE 101
-#define NAME 20
+#define LEN 101
 
 #define BUBBLE_SORT 1
 #define INSERT_SORT 2
@@ -16,95 +15,100 @@
 #define COUNT_SORT  7
 #define BUCKET_SORT 8
 #define RADIX_SORT  9
+#define SHELL_SORT  10
 
 #define BUCKET_AMT 10
-#define RADIX_BIT  10
-#define radix_key(v, b) (v / b % RADIX_BIT);
+#define RADIX_BIT  5
+#define radix_key(v, b) (((v) / (b)) % RADIX_BIT);
 
-int data[SIZE];
+int data[LEN];
 
 int main(void)
 {
     struct timeval start, end;
-    init(data, SIZE);
-    sort_test(data, SIZE, BUBBLE_SORT, "bubble_sort");
-    sort_test(data, SIZE, INSERT_SORT, "insert_sort");
-    sort_test(data, SIZE, SELECT_SORT, "select_sort");
-    sort_test(data, SIZE, MERGE_SORT,  "merge_sort");
-    sort_test(data, SIZE, QUICK_SORT,  "quick_sort");
-    sort_test(data, SIZE, HEAP_SORT,   "heap_sort");
-    sort_test(data, SIZE, COUNT_SORT,  "count_sort");
-    sort_test(data, SIZE, BUCKET_SORT, "bucket_sort");
-    sort_test(data, SIZE, RADIX_SORT,  "radix_sort");
+    init(data, LEN);
+    sort_test(data, LEN, BUBBLE_SORT, "bubble_sort");
+    sort_test(data, LEN, INSERT_SORT, "insert_sort");
+    sort_test(data, LEN, SELECT_SORT, "select_sort");
+    sort_test(data, LEN, MERGE_SORT,  "merge_sort");
+    sort_test(data, LEN, QUICK_SORT,  "quick_sort");
+    sort_test(data, LEN, HEAP_SORT,   "heap_sort");
+    sort_test(data, LEN, COUNT_SORT,  "count_sort");
+    sort_test(data, LEN, BUCKET_SORT, "bucket_sort");
+    sort_test(data, LEN, RADIX_SORT,  "radix_sort");
+    sort_test(data, LEN, SHELL_SORT,  "shell_sort");
 }
 
-int init(int data[], int sum)
+int init(int data[], int len)
 {
     int i = 0;
     srand((int)time(0));
-    for(; i < sum; ++i)
-        data[i] = rand() % sum;
-    printf("init list size:%d\n", SIZE);
-    print(data, SIZE);
+    for(; i < len; ++i)
+        data[i] = rand() % len;
+    printf("init list size:%d\n", LEN);
+    print(data, LEN);
 }
 
-int print(int data[], int sum)
+int print(int data[], int len)
 {
     int i = 0;
-    for(; i < sum; ++i)
+    for(; i < len; ++i)
         printf("%d ", data[i]);
     printf("\n");
 }
 
-sort_test(int data[], int sum, int type, char name[])
+sort_test(int data[], int len, int type, char name[])
 {
-    int tmp[sum];
-    memcpy(tmp, data, sizeof(int) * sum);
+    int tmp[len];
+    memcpy(tmp, data, sizeof(int) * len);
     struct timeval start, end;
     gettimeofday(&start, NULL);
     switch(type)
     {
         case BUBBLE_SORT:
-            bubble_sort(tmp, sum);
+            bubble_sort(tmp, len);
             break;
         case INSERT_SORT:
-            insert_sort(tmp, sum);
+            insert_sort(tmp, len);
             break;
         case SELECT_SORT:
-            select_sort(tmp, sum);
+            select_sort(tmp, len);
             break;
         case MERGE_SORT:
-            merge_sort(tmp, sum);
+            merge_sort(tmp, len);
             break;
         case QUICK_SORT:
-            quick_sort(tmp, sum);
+            quick_sort(tmp, len);
             break;
         case HEAP_SORT:
-            heap_sort(tmp, sum);
+            heap_sort(tmp, len);
             break;
         case COUNT_SORT:
-            count_sort(tmp, sum);
+            count_sort(tmp, len);
             break;
         case BUCKET_SORT:
-            bucket_sort(tmp, sum);
+            bucket_sort(tmp, len);
             break;
         case RADIX_SORT:
-            radix_sort(tmp, sum);
+            radix_sort(tmp, len);
+            break;
+        case SHELL_SORT:
+            shell_sort(tmp, len);
             break;
     }
     gettimeofday(&end, NULL);
     long int cost = 1000000 * (end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec;
     printf("%s cost %ld us\n", name, cost);
-    print(tmp, SIZE);
+    print(tmp, LEN);
 }
 
-int bubble_sort(int data[], int sum)
+int bubble_sort(int data[], int len)
 {
     int i = 0, j = 0;
     int did = 0, tmp = 0;
-    for(; i < sum; i++)                 // O(n)
+    for(; i < len; i++)                 // O(n)
     {
-        for(j = sum - 2; j >= i; j--)   // O(n^2)
+        for(j = len - 2; j >= i; j--)   // O(n^2)
         {
             if(data[j] > data[j + 1])
             {
@@ -116,11 +120,11 @@ int bubble_sort(int data[], int sum)
     }
 }
 
-int insert_sort(int data[], int sum)
+int insert_sort(int data[], int len)
 {
     int i = 0, j = 0;
     int tmp = 0;
-    for(; i < sum; i++)                 // O(n)
+    for(; i < len; i++)                 // O(n)
     {
         tmp = data[i];
         for(j = i; j > 0; j--)          // O(n^2)
@@ -138,14 +142,14 @@ int insert_sort(int data[], int sum)
     }
 }
 
-int select_sort(int data[], int sum)
+int select_sort(int data[], int len)
 {
     int i = 0, j = 0, s = 0;
     int tmp = 0;
-    for(; i < sum; i++)                       // O(n)
+    for(; i < len; i++)                       // O(n)
     {
         tmp = data[i];
-        for(s = i, j = i + 1; j < sum; j++)   // O(n^2)
+        for(s = i, j = i + 1; j < len; j++)   // O(n^2)
         {
             if(data[j] < tmp)
             {
@@ -162,18 +166,18 @@ int select_sort(int data[], int sum)
 }
 
 // [1,4,2,3] -> [1,4], [2,3]
-int merge_sort(int data[], int sum)
+int merge_sort(int data[], int len)
 {
-    int tmp_data[sum];
+    int tmp_data[len];
     int *src_data = data, *dst_data = tmp_data;
     int d = 0, k = 0, i = 0, j = 0, mi = 0, mj = 0, t = 0;
-    for(d = 1; d < sum; d *= 2)         // O(log2n)
+    for(d = 1; d < len; d *= 2)         // O(log2n)
     {
-        for(k = 0; k < sum; k += d * 2) // O(nlog2n)
+        for(k = 0; k < len; k += d * 2) // O(nlog2n)
         {
             i = k, j = k + d, t = k;
-            mi = k + d < sum ? k + d : sum;
-            mj = k + d * 2 < sum ? k + d * 2 : sum;
+            mi = k + d < len ? k + d : len;
+            mj = k + d * 2 < len ? k + d * 2 : len;
             while(i < mi && j < mj)
             {
                 if(src_data[i] < src_data[j])
@@ -197,16 +201,16 @@ int merge_sort(int data[], int sum)
             dst_data = tmp_data;
         }
     }
-    if(dst_data == data) memcpy(data, tmp_data, sizeof(int) * sum);
+    if(dst_data == data) memcpy(data, tmp_data, sizeof(int) * len);
 }
 
-int quick_sort(int data[], int sum)
+int quick_sort(int data[], int len)
 {
-    int start[sum / 2], end[sum / 2];
+    int start[len / 2], end[len / 2];
     int t = 0;
     int i = 0, j = 0;
     int tmp = 0, mid;
-    for(start[t] = 0, end[t] = sum - 1; t >= 0;)
+    for(start[t] = 0, end[t] = len - 1; t >= 0;)
     {
         if(start[t] >= end[t])
         {
@@ -229,24 +233,24 @@ int quick_sort(int data[], int sum)
     }
 }
 
-int heap_sort(int data[], int sum)
+int heap_sort(int data[], int len)
 {
     int index = 0, left = 0, right = 0, itmp = 0, tmp = 0;
     int i = 0;
-    for(i = (sum - 1) / 2; i >= 0; i--)
+    for(i = (len - 1) / 2; i >= 0; i--)
     {
         index = i;
         while(!0)
         {
             itmp = index, left = 2 * index + 1, right = 2 * (index + 1);
-            if (left < sum && data[itmp] < data[left]) itmp = left;
-            if (right < sum && data[itmp] < data[right]) itmp = right;
+            if (left < len && data[itmp] < data[left]) itmp = left;
+            if (right < len && data[itmp] < data[right]) itmp = right;
             if (itmp == index) break;
             tmp = data[index], data[index] = data[itmp], data[itmp] = tmp;
             index = itmp;
         }
     }
-    for(i = sum - 1; i > 0; i--)
+    for(i = len - 1; i > 0; i--)
     {
         tmp = data[0], data[0] = data[i], data[i] = tmp;
         index = 0;
@@ -262,12 +266,12 @@ int heap_sort(int data[], int sum)
     }
 }
 
-int count_sort(int data[], int sum)
+int count_sort(int data[], int len)
 {
     int i = 0, j = 0;
     int max = 0, tmp = 0;
-    int tmp_data[sum];
-    for(i = 0, max = data[0]; i < sum; i++)
+    int tmp_data[len];
+    for(i = 0, max = data[0]; i < len; i++)
     {
         if(data[i] > max)
         {
@@ -276,7 +280,7 @@ int count_sort(int data[], int sum)
     }
     int count_data[max + 1];
     memset(count_data, 0, sizeof(int) * (max + 1));
-    for(i = 0; i < sum; i++)
+    for(i = 0; i < len; i++)
     {
         count_data[data[i]]++;
     }
@@ -284,20 +288,19 @@ int count_sort(int data[], int sum)
     {
         count_data[i] += count_data[i - 1];
     }
-    for(i = sum - 1; i >= 0; i--)
+    for(i = len - 1; i >= 0; i--)
     {
         tmp = data[i];
-        tmp_data[count_data[tmp] - 1] = tmp;
-        count_data[tmp]--;
+        tmp_data[count_data[tmp]-- - 1] = tmp;
     }
-    memcpy(data, tmp_data, sizeof(int) * sum);
+    memcpy(data, tmp_data, sizeof(int) * len);
 }
 
-int bucket_sort(int data[], int sum)
+int bucket_sort(int data[], int len)
 {
     int i = 0, j = 0;
     int max = 0;
-    for(i = 0, max = data[0]; i < sum; i++)
+    for(i = 0, max = data[0]; i < len; i++)
     {
         if(data[i] > max)
         {
@@ -307,7 +310,7 @@ int bucket_sort(int data[], int sum)
     int tmp_data[BUCKET_AMT][max]; // link_list
     int tmp_count[BUCKET_AMT];
     memset(tmp_count, 0, sizeof(int) * BUCKET_AMT);
-    for(i = 0; i < sum; i++)
+    for(i = 0; i < len; i++)
     {
         j = data[i] * BUCKET_AMT / (max + 1);
         tmp_data[j][tmp_count[j]++] = data[i];
@@ -321,46 +324,79 @@ int bucket_sort(int data[], int sum)
     }
 }
 
-int radix_sort(int data[], int sum)
+int radix_sort(int data[], int len)
 {
     int i = 0, j = 0, k = 0;
     int max = 0, max_key = 0, tmp = 0;
-    for(i = 0, max = data[0]; i < sum; i++)
+    int tmp_data[len];
+    for(i = 0, max = data[0]; i < len; i++)
     {
         if(data[i] > max)
         {
             max = data[i];
         }
     }
-    int tmp_data[BUCKET_AMT][max]; // link_list
-    int tmp_count[BUCKET_AMT];
+    int *src_data = data, *dst_data = tmp_data;
     for(k = 1; k <= max; k *= RADIX_BIT)
     {
-        max_key = radix_key(data[0], k);
-        for(i = 0; i < sum; i++)
+        int bucket[RADIX_BIT] = {0};
+        max_key = radix_key(src_data[0], k);
+        for(i = 0; i < len; i++)
         {
-            tmp = radix_key(data[i], k);
+            tmp = radix_key(src_data[i], k);
             if(tmp > max_key)
             {
                 max_key = tmp;
             }
         }
-        memset(tmp_count, 0, sizeof(int) * BUCKET_AMT);
-        for(i = 0; i < sum; i++)
+        for(i = 0; i < len; i++)
         {
-            tmp = radix_key(data[i], k);
-            j = tmp * BUCKET_AMT / (max_key + 1);
-            tmp_data[j][tmp_count[j]++] = data[i];
+            tmp = radix_key(src_data[i], k);
+            bucket[tmp]++;
         }
-        for(i = 0, j = 0; j < BUCKET_AMT; j++)
+        for(i = 1; i <= max_key; i++)
         {
-            int count = tmp_count[j];
-            if (count > 0)
+            bucket[i] += bucket[i - 1];
+        }
+        for(i = len - 1; i >= 0; i--)
+        {
+            tmp = radix_key(src_data[i], k);
+            dst_data[bucket[tmp]-- - 1] = src_data[i];
+        }
+        if(src_data == data)
+        {
+            src_data = tmp_data;
+            dst_data = data;
+        } else
+        {
+            src_data = data;
+            dst_data = tmp_data;
+        }
+    }
+    if(dst_data == data) memcpy(data, tmp_data, sizeof(int) * len);
+}
+
+int shell_sort(int data[], int len)
+{
+    int i = 0, j = 0, k = 0;
+    int tmp = 0;
+    for(k = len >> 1; k > 0; k >>= len)
+    {
+        for(i = 0; i < len; i += k)
+        {
+            tmp = data[i];
+            for(j = i; j > 0; j -= k)
             {
-                count_sort(tmp_data[j], count);
-                memcpy(&data[i], tmp_data[j], sizeof(int) * count);
-                i += count;
+                if(tmp < data[j - k])
+                {
+                    data[j] = data[j - k];
+                } else
+                {
+                    data[j] = tmp;
+                    break;
+                }
             }
+            if (j == 0) data[0] = tmp;
         }
     }
 }
